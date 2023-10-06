35dfc1844ba3b17e54f73c33d77be183c2ad9efc
psychlone77 <nithikasdias@gmail.com>
2023-10-06 15:57:10 +0530

    SQL files  added in new folder.

---
+++ SQL_files/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO `b_airways`.`organizational_info`
+(`airline_name`,
+`airline_hotline`,
+`airline_email`,
+`address`,
+`airline_account_no`)
+VALUES
+('B Airways',
+'+9411123456',
+'customersupport@bairways.com',
+'No1, Colombo, Sri Lanka',
+'123456789');
