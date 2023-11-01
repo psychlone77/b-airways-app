3f90632c62b917b835221398e4735c79ab9e3fde
disini-thev <ma17disini@gmail.com>
2023-11-01 15:34:26 +0530

    changed report 1

--- SQL_files/reports.sql
+++ SQL_files/reports.sql
@@ -3,9 +3,9 @@
 -- passenger_info view is created for this
 
 -- Create the procedure to get passengers above 18 in a flight
-DROP PROCEDURE IF EXISTS get_passengers_above_18_in_flight;
+DROP PROCEDURE IF EXISTS get_passengers_in_flight;
     DELIMITER |
-    CREATE PROCEDURE get_passengers_above_18_in_flight(IN val_route_id VARCHAR(10))
+    CREATE PROCEDURE get_passengers_in_flight(IN val_route_id VARCHAR(10))
     BEGIN
         DECLARE s_id INT;
         DECLARE birth_date DATE;
@@ -17,44 +17,18 @@ DROP PROCEDURE IF EXISTS get_passengers_above_18_in_flight;
         ORDER BY scheduled_departure ASC
         LIMIT 1;
         
-        SELECT * from passenger_info
-        where user_id 
-        in (
-            SELECT user_id
-            FROM User_Booking
-            WHERE schedule_id = s_id AND calculateAge(birth_date) > 18);
+        SELECT name, passport_no,
+        CASE
+           WHEN calculateAge(passenger_info.birth_date) >= 18 THEN 'Above 18'
+           ELSE 'Below 18'
+        END AS age_group
+        FROM passenger_info
+        LEFT JOIN User_Booking ON passenger_info.user_id = User_Booking.user_id
+        WHERE User_Booking.schedule_id = s_id
+        order by age_group desc;
     END 
     |
     DELIMITER ;
-
--- Create the procedure to get passengers below 18 in a flight
-DROP PROCEDURE IF EXISTS get_passengers_below_18_in_flight;
-    DELIMITER |
-    CREATE PROCEDURE get_passengers_below_18_in_flight(IN val_route_id VARCHAR(10))
-    BEGIN
-        DECLARE s_id INT;
-        DECLARE birth_date DATE;
-        
-        -- Find the next immediate flight for the given route
-        SELECT schedule_id INTO s_id
-        FROM Scheduled_Flight
-        WHERE route_id = val_route_id AND scheduled_departure > NOW()
-        ORDER BY scheduled_departure ASC
-        LIMIT 1;
-        
-        SELECT * from passenger_info
-        where user_id 
-        in (
-            SELECT user_id
-            FROM User_Booking
-            WHERE schedule_id = s_id AND calculateAge(birth_date) < 18);
-    END 
-    |
-    DELIMITER ;
-
-
-
-
 
 -- Report 1 (not 100% sure, have to check)
 
