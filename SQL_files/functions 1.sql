bfd9ccbc6ca07f5a061e57a9ebd90d51f0cd199e
Nithika Dias <nithikasdias@gmail.com>
2023-11-03 10:36:59 +0530

    Function removed

--- SQL_files/functions 1.sql
+++ SQL_files/functions 1.sql
@@ -68,9 +68,9 @@ DROP FUNCTION IF EXISTS calculateTotalPrice;
     DELIMITER ;
 
 --  check this trigger
-DROP TRIGGER IF EXISTS isnert_before_booking;
+DROP TRIGGER IF EXISTS insert_before_booking;
     DELIMITER |
-    CREATE TRIGGER isnert_before_booking
+    CREATE TRIGGER insert_before_booking
     BEFORE INSERT ON User_Booking
     FOR EACH ROW
     BEGIN
@@ -498,26 +498,3 @@ DROP PROCEDURE IF EXISTS get_booked_seat_info_by_class;
     DELIMITER ;
 
 DELIMITER //
-
-DROP FUNCTION IF EXISTS discount;
-
-CREATE FUNCTION discount(userId INT) RETURNS VARCHAR(255)
-DETERMINISTIC
-BEGIN
-    DECLARE discount_and_level VARCHAR(255);
-
-    SELECT
-        CONCAT(
-            'Discount: ',
-            COALESCE(discount_percentage, '0%'),
-            ', Level: ',
-            COALESCE(category, 'Guest')
-        )
-    INTO discount_and_level
-    FROM discounts
-    WHERE user_id = userId;
-
-    RETURN discount_and_level;
-END//
-
-DELIMITER ;
